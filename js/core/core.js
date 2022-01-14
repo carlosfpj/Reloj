b4c1abb96563fd80f83e3b1dcdcc321f6b12d312
carlosfpj <cafer89@gmail.com>
2022-01-13 22:32:24 -0500

    time displayed

---
+++ js/core/core.js
@@ -0,0 +1,15 @@
+export default function init(){
+  var interval = setInterval(displayTime, 1000)
+  console.log(interval);
+}
+
+function displayTime() {
+  const rawDate = new Date() * 1000
+  const hours = rawDate.getHours()
+  const minutes = rawDate.getMinutes()
+  const seconds = rawDate.getSeconds()
+  console.log("la hora es: " + hours + " horas\n" + minutes + " minutos\n" + seconds + " segundos");
+  const display = document.querySelector('.display')
+  const template = `${hours}:${minutes}:${seconds}`
+  display.textContent = template;
+}
