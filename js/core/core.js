f87bd6584438f6253a3462e68d18a10b5c5075a5
carlosfpj <cafer89@gmail.com>
2022-01-16 18:06:47 -0500

    more css styles added

--- js/core/core.js
+++ js/core/core.js
@@ -4,11 +4,12 @@ export default function init(){
 }
 
 function displayTime() {
-  const rawDate = new Date() * 1000
+  const rawDate = new Date()
+  console.log(rawDate);
   const hours = rawDate.getHours()
   const minutes = rawDate.getMinutes()
   const seconds = rawDate.getSeconds()
-  console.log("la hora es: " + hours + " horas\n" + minutes + " minutos\n" + seconds + " segundos");
+  // console.log("la hora es: " + hours + " horas\n" + minutes + " minutos\n" + seconds + " segundos");
   const display = document.querySelector('.display')
   const template = `${hours}:${minutes}:${seconds}`
   display.textContent = template;
